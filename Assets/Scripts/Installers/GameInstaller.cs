3213b8d70e5f25999bfd8505818541c499ac079b
PolinaMakarova <iampolya8@gmail.com>
2022-06-13 14:39:45 +0300

    Added plugins

---
+++ Assets/Scripts/Installers/GameInstaller.cs
@@ -0,0 +1,32 @@
+using GameLogics.FieldLogics;
+using Services.Cockroach;
+using Services.Game;
+using Services.Stats;
+using Services.UI;
+using Services.Updater;
+using Settings;
+using UnityEngine;
+using Views;
+using Zenject;
+
+public class GameInstaller : MonoInstaller
+{
+    [SerializeField]
+    private GameObject _settings;
+    [SerializeField]
+    private GameObject _playerPrefab;
+    public override void InstallBindings()
+    {
+        Container.Bind<SettingsContainer>().FromComponentOn(_settings).AsSingle();
+        Container.Bind<UpdateService>().FromNewComponentOnNewGameObject().AsSingle();
+        Container.Bind<GameStateService>().AsSingle();
+        Container.Bind<GameLoopManager>().AsSingle().NonLazy();
+        Container.Bind<UIService>().AsSingle();
+        
+        Container.Bind<PlayerController>().AsSingle();
+        Container.Bind<PlayerView>().FromComponentInNewPrefab(_playerPrefab).AsSingle().NonLazy();
+        
+        Container.BindInterfacesTo<StatsService>().AsSingle();
+        Container.BindInterfacesTo<CockroachManager>().AsSingle();
+    }
+}
